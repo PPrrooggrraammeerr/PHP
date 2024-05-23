f69918176528d924b98fbc05ca72fe05ddf3f002
Black coffee <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2024-05-22 23:10:54 -0300

    Atualizar o acesso.php

--- sessoes/acesso.php
+++ sessoes/acesso.php
@@ -15,7 +15,7 @@ if (isset($_SESSION['email_session'])) {
 <head>
 	<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sessões</title>
+    <title>Acesso</title>
 	<?php
 	echo '<style>
 		.sair {
