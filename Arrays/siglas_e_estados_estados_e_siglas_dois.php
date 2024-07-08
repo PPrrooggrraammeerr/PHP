83eb12fc527c715c51576fc17e0c5c0d57f0e66d
Black coffee <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2024-07-07 22:38:01 -0300

    Add files via upload

---
+++ Arrays/siglas_e_estados_estados_e_siglas_dois.php
@@ -0,0 +1,126 @@
+<?php
+
+$estados_brasileiros = [
+    'AC' => 'Acre',
+    'AL' => 'Alagoas',
+    'AP' => 'Amapá',
+    'AM' => 'Amazonas',
+    'BA' => 'Bahia',
+    'CE' => 'Ceará',
+    'DF' => 'Distrito Federal',
+    'ES' => 'Espírito Santo',
+    'GO' => 'Goiás',
+    'MA' => 'Maranhão',
+    'MT' => 'Mato Grosso',
+    'MS' => 'Mato Grosso do Sul',
+    'MG' => 'Minas Gerais',
+    'PA' => 'Pará',
+    'PB' => 'Paraíba',
+    'PR' => 'Paraná',
+    'PE' => 'Pernambuco',
+    'PI' => 'Piauí',
+    'RJ' => 'Rio de Janeiro',
+    'RN' => 'Rio Grande do Norte',
+    'RS' => 'Rio Grande do Sul',
+    'RO' => 'Rondônia',
+    'RR' => 'Roraima',
+    'SC' => 'Santa Catarina',
+    'SP' => 'São Paulo',
+    'SE' => 'Sergipe',
+    'TO' => 'Tocantins'
+];
+
+$siglas_estados_brasileiros = [
+    'Acre' => 'AC',
+    'Alagoas' => 'AL',
+    'Amapá' => 'AP',
+    'Amazonas' => 'AM',
+    'Bahia' => 'BA',
+    'Ceará' => 'CE',
+    'Distrito Federal' => 'DF',
+    'Espírito Santo' => 'ES',
+    'Goiás' => 'GO',
+    'Maranhão' => 'MA',
+    'Mato Grosso' => 'MT',
+    'Mato Grosso do Sul' => 'MS',
+    'Minas Gerais' => 'MG',
+    'Pará' => 'PA',
+    'Paraíba' => 'PB',
+    'Paraná' => 'PR',
+    'Pernambuco' => 'PE',
+    'Piauí' => 'PI',
+    'Rio de Janeiro' => 'RJ',
+    'Rio Grande do Norte' => 'RN',
+    'Rio Grande do Sul' => 'RS',
+    'Rondônia' => 'RO',
+    'Roraima' => 'RR',
+    'Santa Catarina' => 'SC',
+    'São Paulo' => 'SP',
+    'Sergipe' => 'SE',
+    'Tocantins' => 'TO'
+];
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Siglas e Estados | Estados e Siglas</title>
+    <style type="text/css">
+        body {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .container {
+            display: flex;
+            gap: 4px;
+        }
+
+        td {
+            padding-left: 4px;
+            padding-right: 4px;
+        }
+    </style>
+</head>
+<body>
+    <h2>Siglas e Estados | Estados e Siglas</h2>
+    <div class="container">
+        <div>
+        <table border="1px">
+            <tr>
+                <th>Sigla</th>
+                <th>Estado</th>
+            </tr>
+            <?php foreach ($estados_brasileiros as $key => $estado_brasileiro) {
+            ?>
+            <tr>
+                <td><?php echo $key;?></td>
+                <td><?php echo $estado_brasileiro;?></td>
+            </tr>
+            <?php
+            }
+            ?>
+        </table>
+        </div>
+        <div>
+        <table border="1px">
+            <tr>
+                <th>Estado</th>
+                <th>Sigla</th>
+            </tr>
+            <?php foreach ($siglas_estados_brasileiros as $key => $sigla_estado_brasileiro) {
+            ?>
+            <tr>
+                <td><?php echo $key;?></td>
+                <td><?php echo $sigla_estado_brasileiro;?></td>
+            </tr>
+            <?php
+            }
+            ?>
+        </table>
+        </div>
+    </div>
+</body>
+</html>
---
+++ Arrays/siglas_e_estados_estados_e_siglas_tres.php
@@ -0,0 +1,98 @@
+<?php
+
+$siglas_e_estados_brasileiros = [
+    ['sigla' => 'AC', 'estado' => 'Acre'],
+    ['sigla' => 'AL', 'estado' => 'Alagoas'],
+    ['sigla' => 'AP', 'estado' => 'Amapá'],
+    ['sigla' => 'AM', 'estado' => 'Amazonas'],
+    ['sigla' => 'BA', 'estado' => 'Bahia'],
+    ['sigla' => 'CE', 'estado' => 'Ceará'],
+    ['sigla' => 'DF', 'estado' => 'Distrito Federal'],
+    ['sigla' => 'ES', 'estado' => 'Espírito Santo'],
+    ['sigla' => 'GO', 'estado' => 'Goiás'],
+    ['sigla' => 'MA', 'estado' => 'Maranhão'],
+    ['sigla' => 'MT', 'estado' => 'Mato Grosso'],
+    ['sigla' => 'MS', 'estado' => 'Mato Grosso do Sul'],
+    ['sigla' => 'MG', 'estado' => 'Minas Gerais'],
+    ['sigla' => 'PA', 'estado' => 'Pará'],
+    ['sigla' => 'PB', 'estado' => 'Paraíba'],
+    ['sigla' => 'PR', 'estado' => 'Paraná'],
+    ['sigla' => 'PE', 'estado' => 'Pernambuco'],
+    ['sigla' => 'PI', 'estado' => 'Piauí'],
+    ['sigla' => 'RJ', 'estado' => 'Rio de Janeiro'],
+    ['sigla' => 'RN', 'estado' => 'Rio Grande do Norte'],
+    ['sigla' => 'RS', 'estado' => 'Rio Grande do Sul'],
+    ['sigla' => 'RO', 'estado' => 'Rondônia'],
+    ['sigla' => 'RR', 'estado' => 'Roraima'],
+    ['sigla' => 'SC', 'estado' => 'Santa Catarina'],
+    ['sigla' => 'SP', 'estado' => 'São Paulo'],
+    ['sigla' => 'SE', 'estado' => 'Sergipe'],
+    ['sigla' => 'TO', 'estado' => 'Tocantins']
+];
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Siglas e Estados | Estados e Siglas</title>
+    <style type="text/css">
+        body {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .container {
+            display: flex;
+            gap: 4px;
+        }
+
+        td {
+            padding-left: 4px;
+            padding-right: 4px;
+        }
+    </style>
+</head>
+<body>
+    <h2>Siglas e Estados | Estados e Siglas</h2>
+	<div class="container">
+        <div>
+        <table border="1px">
+            <tr>
+                <th>Sigla</th>
+                <th>Estado</th>
+            </tr>
+            <?php 
+            foreach ($siglas_e_estados_brasileiros as $sigla_e_estado_brasileiro) {
+            ?>
+            <tr>
+                <td><?php echo $sigla_e_estado_brasileiro['sigla'];?></td>
+                <td><?php echo $sigla_e_estado_brasileiro['estado'];?></td>
+            </tr>   
+            <?php
+            }
+            ?>
+        </table>
+        </div>
+        <div>
+		<table border="1px">
+			<tr>
+				<th>Estado</th>
+				<th>Sigla</th>
+			</tr>
+			<?php 
+			foreach ($siglas_e_estados_brasileiros as $sigla_e_estado_brasileiro) {
+			?>
+			<tr>
+				<td><?php echo $sigla_e_estado_brasileiro['estado'];?></td>
+                <td><?php echo $sigla_e_estado_brasileiro['sigla'];?></td>
+			</tr>	
+			<?php
+		    }
+		    ?>
+		</table>
+        </div>
+	</div>
+</body>
+</html>
